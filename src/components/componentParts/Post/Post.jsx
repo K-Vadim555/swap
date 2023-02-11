94973c9ac72c899e0c1e66492c8dc16b46e1403e
unknown <sasha.kara.16@bk.ru>
2023-02-11 10:55:02 +0200

    commit

--- src/components/componentParts/Post/Post.jsx
+++ src/components/componentParts/Post/Post.jsx
@@ -293,7 +293,7 @@ export default function Post({setValue, setValue1, value , value1, afn1 , afn ,s
                                 navigator.clipboard.writeText(post.v);
                                 }}
                             >
-                              <span>{post.v .slice(0,10)+ '...'}</span> <img src={copy} alt="" />
+                              <span>{post.v .slice(0,20)+ '...'}</span> <img src={copy} alt="" />
                              
                         </button>
                          <Qr value={post.v} />
@@ -317,7 +317,7 @@ export default function Post({setValue, setValue1, value , value1, afn1 , afn ,s
                                 navigator.clipboard.writeText(post.v);
                                 }}
                             >
-                              <span>{post.v .slice(0,10)+ '...' }</span> <img src={copy} alt="" />
+                              <span>{post.v .slice(0,20)+ '...' }</span> <img src={copy} alt="" />
                              
                         </button>
                         <p className='ex'>The exchange rate will be fixed after receiving 1 network confirmations.</p>
